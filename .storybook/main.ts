c14a4b0c32c8cd952fe263d31d2f4fc2220d07ad
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-08-12 18:49:45 +0100

    Adds staticDirs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,12 @@ import type { StorybookConfig } from "@storybook/svelte-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|ts|svelte)"],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-svelte-csf",
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
   ],
   framework: {
     name: "@storybook/svelte-vite",
